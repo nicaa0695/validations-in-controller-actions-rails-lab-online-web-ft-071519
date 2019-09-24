fc1ab3e5ad5e9322d80fb82d31b1badfa0826b35
Veronica Lopez <nicaa0695@gmail.com>
2019-09-24 19:16:28 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true 
+  validates :content, length: { minimum: 100 }
   
-  
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  # validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
